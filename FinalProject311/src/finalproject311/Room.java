b6a64c167315a70529798332b7e6776acd214b00
Graham <Graham@130.203.89.86>
2016-04-25 15:36:24 -0400

    added divider table and trashcan and updated room class

--- FinalProject311/src/finalproject311/Room.java
+++ FinalProject311/src/finalproject311/Room.java
@@ -7,6 +7,7 @@ package finalproject311;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -21,68 +22,104 @@ import javax.swing.Timer;
  */
 public class Room extends JPanel implements ActionListener, KeyListener{
     
+    
+    public static final int PodeumWidth = 60;
+    public static final int PodeumHeight = 15;
+    public static final int LongtableWidth = 175;
+    public static final int MiddletableWidth = 90;
+    public static final int ShorttableWidth = 18;
+    public static final int TableHeight = 15;
+    public static final int FrontTableWidth = 30;
+    public static final int FrontTableHeight = 64;
+    public static final int WallUDWidth = 450;
+    public static final int WallUDHeight = 45;
+    public static final int WallLRWidth= 15;
+    public static final int WallLRHeight = 600;
+
+    private Rectangle podeum;
+    private Rectangle frontTable;
+    private Rectangle leftDesk1;
+    private Rectangle leftDesk2;
+    private Rectangle leftDesk3;
+    private Rectangle leftDesk4;
+    private Rectangle rightDesk1;
+    private Rectangle rightDesk2;
+    private Rectangle rightDesk3;
+    private Rectangle rightDesk4;
+    private Rectangle printer;
+    private Rectangle trashcan;
+    private Rectangle key;
+    private Rectangle wallUD1;
+    private Rectangle wallUD2;
+    private Rectangle wallLR1;
+    private Rectangle wallLR2;
+       
     private int roomNumber;
-    private Timer timer1;
-    private String currentClass;
-    private ArrayList<Desk>  desks;
+    private ArrayList<Rectangle>  roomObjects;
     private ArrayList<Object> computers;
     private Image roomImage;
     private player player1;
+    private Timer timer1;
     //private GameFrame theFrame;
     
     public Room(int theRoomNumber){
        super();
        this.addKeyListener(this);
-       this.player1 = new player(500, 500);
-       
-       //this.theFrame = new GameFrame(this);
+       this.player1 = new player();
        this.setFocusable(true);
        this.roomNumber =  theRoomNumber;
-       this.currentClass = "";
-       this.desks = new ArrayList<Desk>();
-       populateDesks();
+       this.roomObjects = new ArrayList<Rectangle>();
        this.timer1 = new Timer(50, this);
        this.timer1.start();
        this.computers = new ArrayList<Object>();
        
     }
 
-    public final void populateDesks(){
-        
-            
-            Desk newDesk = new Desk(50, 50,100, 50, "Desk1");
-            Desk newDesk2 = new Desk(50, 50,100, 120, "Desk2");
-            Desk newDesk3 = new Desk(50, 50,100, 190, "Desk3");
-            Desk newDesk4 = new Desk(50, 50,100, 260, "Desk4");
-            Desk newDesk5 = new Desk(50, 50,100, 330, "Desk5");
-            Desk newDesk6 = new Desk(50, 50,100, 400, "Desk6");
-            Desk newDesk7 = new Desk(50, 50,100, 5470, "Desk7");
-            Desk newRowDesk = new Desk(50, 50,300, 50, "Desk1");
-            Desk newRowDesk2 = new Desk(50, 50,300, 120, "Desk2");
-            Desk newRowDesk3 = new Desk(50, 50,300, 190, "Desk3");
-            Desk newRowDesk4 = new Desk(50, 50,300, 260, "Desk4");
-            Desk newRowDesk5 = new Desk(50, 50,300, 330, "Desk5");
-            Desk newRowDesk6 = new Desk(50, 50,300, 400, "Desk6");
-            Desk newRowDesk7 = new Desk(50, 50,300, 5470, "Desk7");
-            desks.add(newDesk);
-            desks.add(newDesk2);  
-            desks.add(newDesk3);  
-            desks.add(newDesk4);  
-            desks.add(newDesk5);
-            desks.add(newDesk6);  
-            desks.add(newDesk7);
-            desks.add(newRowDesk);
-            desks.add(newRowDesk2);  
-            desks.add(newRowDesk3);  
-            desks.add(newRowDesk4);  
-            desks.add(newRowDesk5);
-            desks.add(newRowDesk6);  
-            desks.add(newRowDesk7);
-            
-            
-        
+    private void placeStations() {
+        player1.setBounds(450, 150, 50, 50);
+        podeum.setBounds(43, 100, PodeumWidth, PodeumHeight);
+        frontTable.setBounds(363,100,PodeumWidth, PodeumHeight);
+        leftDesk1.setBounds(0, 80, LongtableWidth, TableHeight);
+        leftDesk2.setBounds(0,105,LongtableWidth, TableHeight);
+        leftDesk3.setBounds(266,256,LongtableWidth, TableHeight);
+        leftDesk4.setBounds(94,356,LongtableWidth, TableHeight);
+        rightDesk1.setBounds(266,356,LongtableWidth, TableHeight);
+        rightDesk2.setBounds(94,456,LongtableWidth, TableHeight);
+        rightDesk3.setBounds(266,456,LongtableWidth, TableHeight);
+        rightDesk4.setBounds(436, 449, LongtableWidth, TableHeight);
+        printer.setBounds(444, 353, FrontTableWidth, FrontTableHeight);
+        trashcan.setBounds(413, 449, FrontTableWidth, FrontTableHeight);
+        key.setBounds(29, 353, FrontTableWidth, FrontTableHeight);
+        wallUD1.setBounds(23,30, WallUDWidth, WallUDHeight);
+        wallUD2.setBounds(23, 544, WallUDWidth, WallUDHeight);
+        wallLR1.setBounds(0,66, WallLRWidth, WallLRHeight);
+        wallLR2.setBounds(458,226, WallLRWidth, WallLRHeight);
+   
     }
     
+    private void init() {
+        podeum = new Rectangle();
+        frontTable = new Rectangle();
+        leftDesk1 = new Rectangle();
+        leftDesk2 = new Rectangle();
+        leftDesk3 = new Rectangle();
+        leftDesk4 = new Rectangle();
+        rightDesk1 = new Rectangle();
+        rightDesk2 = new Rectangle();
+        rightDesk3 = new Rectangle();
+        rightDesk4 = new Rectangle();
+        printer = new Rectangle();
+        trashcan = new Rectangle();
+        key = new Rectangle();
+        wallUD1 = new Rectangle();
+        wallUD2 = new Rectangle();
+        wallLR1 = new Rectangle();
+        wallLR2 = new Rectangle();
+
+   
+        
+        
+    }
     /**
      * @return the roomNumber
      */
@@ -98,31 +135,17 @@ public class Room extends JPanel implements ActionListener, KeyListener{
     }
 
     /**
-     * @return the currentClass
+     * @return the roomObjects
      */
-    public String getCurrentClass() {
-        return currentClass;
+    public ArrayList<Rectangle> getRoomObjects() {
+        return roomObjects;
     }
 
     /**
-     * @param currentClass the currentClass to set
+     * @param roomObjects the roomObjects to set
      */
-    public void setCurrentClass(String currentClass) {
-        this.currentClass = currentClass;
-    }
-
-    /**
-     * @return the desks
-     */
-    public ArrayList<Desk> getDesks() {
-        return desks;
-    }
-
-    /**
-     * @param desks the desks to set
-     */
-    public void setDesks(ArrayList<Desk> desks) {
-        this.desks = desks;
+    public void setRoomObjects(ArrayList<Rectangle> desks) {
+        this.roomObjects = desks;
     }
 
     /**
@@ -150,9 +173,9 @@ public class Room extends JPanel implements ActionListener, KeyListener{
     }
     
      public void paintDesks(Graphics g){
-      for(int i = 0; i<desks.size(); i++)
+      for(int i = 0; i<roomObjects.size(); i++)
        {
-           desks.get(i).paintComponent(g);
+           roomObjects.get(i).paintComponent(g);
        }
     }
      
---
+++ FinalProject311/src/finalproject311/Trashcan.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject311;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Graham
+ */
+class Trashcan extends Rectangle{
+    private int dy;
+    private final Dimension size;
+    private Image trashcanImage;
+    private String deskID;
+    
+    
+    public Trashcan(int panelHeight, int panelWidth,int deskx,int desky, String Name) {
+        
+        
+        this.deskID = Name;
+        this.size = new Dimension(panelWidth, panelHeight);
+        this.setBounds(deskx, desky, panelHeight, panelWidth);  
+      
+    }
+    
+    
+    public void paintComponent(Graphics g){
+        move();
+        g.setColor(Color.MAGENTA);
+        g.draw3DRect(this.x, this.y, this.width, this.height, true);
+        g.fill3DRect(x, y, width, height, true);
+    }
+    
+    public double getCurrentX() {
+        return super.getX();
+    }
+    
+    public double getCurrrentY() {
+        return super.getY();
+    }
+    
+  
+    public void move() {
+        
+        
+    }
+    
+}
+
