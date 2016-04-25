5864289595987dd6157e8c59915aeeb3cbcb5522
matthewtucker <matthewtucker@client-104-39-91-17.mobility.psu.edu>
2016-04-25 14:59:47 -0400

    Added old code

---
+++ FinalProject311/src/finalproject311/player.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject311;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author matthewtucker
+ */
+public class player extends Rectangle {
+       
+
+        private int dx;
+        private int dy;
+
+        public player() {
+            this.dx = 0;
+            this.dy = 0;
+            this.setBounds(200, 50, 30, 30);
+        }
+
+        public void move() {
+            this.x += dx;
+            this.y += dy;
+        }
+
+        public void setDx(int dx) {
+            this.dx = dx;
+        }
+
+        public void setDy(int dy) {
+            this.dy = dy;
+        }
+
+        public void paintComponent(Graphics g) {
+            move();
+            g.drawRect(x, y, 30, 30);
+        }
+    
+}
